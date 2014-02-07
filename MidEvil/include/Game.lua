f98bf64dd514a36bab85704af3ca623d06368d79
jbooth <jbooth@maku.ac>
2014-02-07 13:48:21 -0700

    Added come input for chat. Its in debug mode for now!

---
+++ MidEvil/include/Game.lua
@@ -0,0 +1,17 @@
+Game = {}
+Game = {	menu = true, 
+		game = false 
+		}
+
+function Game.load()
+
+end
+
+function Game.update()
+
+end
+
+
+function Game.draw()
+
+end
--- MidEvil/main.lua
+++ MidEvil/main.lua
@@ -3,16 +3,32 @@ tilecollider = require 'include.tilecollider'
 require 'include.map'
 require 'include.player'
 require 'include.hud'
+require 'include.Game'
 -------------------------------------------------------------------------------
 
 function love.load()
 	cam = gamera.new(0,0,mapWidth,mapHeight)
+	love.keyboard.setKeyRepeat( enable )
+	backspacedtcount = 0
+	text = ">"
 end
 
+function love.textinput(t)
+    text = text .. t
+end
 
 function love.update(dt)
 	p1:move(dt)
 	cam:setPosition(p1.x, p1.y)
+	if Game.menu then
+        	back = love.keyboard.isDown( "backspace" )
+        	if back and backspacedtcount > 0.10 then
+                	text = text:sub(1, #text - 1)
+                	backspacedtcount = 0
+        	elseif back then
+                	backspacedtcount = backspacedtcount + dt
+        	end
+	end
 end
 
 
@@ -24,4 +40,5 @@ function love.draw()
 	end)
 	hud_draw()
 	--love.graphics.print("Speed: " .. p1.v, 20, 20) -- debug
+	love.graphics.print(text, 20, 30) -- debug
 end
